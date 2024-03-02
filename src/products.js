173deb175a490d70b947e71ca027085a9d8e633c
nebiyattakele <nebiyattakele23@gmail.com>
2024-03-02 10:45:55 +0300

    first commit

--- src/products.js
+++ src/products.js
@@ -1,59 +1,59 @@
-import product1 from "./assets/products/1.png";
-import product2 from "./assets/products/2.png";
-import product3 from "./assets/products/3.png";
-import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
-import product6 from "./assets/products/6.webp";
-import product7 from "./assets/products/7.webp";
-import product8 from "./assets/products/8.webp";
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
 
 export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPhone",
-    price: 999.0,
+    productName: "shoes1",
+    price: 789.0,
     productImage: product1,
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
+    productName: "shoes2",
     price: 1999.0,
     productImage: product2,
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
-    price: 699.0,
+    productName: "shoes3",
+    price: 786.0,
     productImage: product3,
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
+    productName: "shoes4",
     price: 228.0,
     productImage: product4,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
-    price: 19.99,
+    productName: "shoes5",
+    price: 78.99,
     productImage: product5,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
+    productName: "shoes6",
+    price: 85.0,
     productImage: product6,
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    productName: "shoes7",
     price: 120.0,
     productImage: product7,
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
+    productName: "shoes8",
+    price: 49.0,
     productImage: product8,
   },
 ];
